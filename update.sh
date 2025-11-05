4c60011acd46930f1cc2f2a6ee7c17a3931ab08c
REVO9 <luisdanker@gmx.de>
2025-11-05 22:10:12 +0100

    update.sh script

--- update.sh
+++ update.sh
@@ -1,4 +1,23 @@
 #!/usr/bin/env bash
 
-nix flake update /etc/nixos/
-sudo nixos-rebuild switch --flake /etc/nixos/
+sudo echo ""
+
+FLAKE_PATH=/etc/nixos/
+FLAKE_LOCK_PATH=$FLAKE_PATH/flake.lock
+FLAKE_LOCK_BAK_PATH=$FLAKE_PATH/flake.lock.bak
+
+cp $FLAKE_LOCK_PATH $FLAKE_LOCK_BAK_PATH || exit 1
+
+nix flake update --flake $FLAKE_PATH
+
+sudo nixos-rebuild switch --flake $FLAKE_PATH || abort
+git commit flake.lock -m "updated flake" || abort
+rm $FLAKE_LOCK_BAK_PATH
+echo "Success"
+exit 0
+
+abort() {
+    echo "FATAL nixos-rebuild switch failed. Restoring flake.lock" >&2
+    mv -f $FLAKE_LOCK_BAK_PATH $FLAKE_LOCK_PATH || exit 1
+    exit 1
+}
